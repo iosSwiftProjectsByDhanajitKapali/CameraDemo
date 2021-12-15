301ab5c06d8367b6df09b3ce1d3d084d3940325d
Dhanajit Kapali <dhanajit.kapali@unthinkable.co>
2021-12-15 18:38:56 +0530

    Initial Commit

---
+++ CameraDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraDemo
+//
+//  Created by unthinkable-mac-0025 on 15/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
