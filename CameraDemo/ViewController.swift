03054fd99943a6ac487001e99a1d17da19882d4f
Dhanajit Kapali <dhanajit.kapali@unthinkable.co>
2021-12-16 11:01:00 +0530

    added feature to click photo

--- CameraDemo/ViewController.swift
+++ CameraDemo/ViewController.swift
@@ -6,14 +6,117 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //1. Camera Session
+    var session : AVCaptureSession?
+    
+    //2.Photo Output
+    let output = AVCapturePhotoOutput()
+    
+    //3.Video Preview
+    let previewLayer = AVCaptureVideoPreviewLayer()
+    
+    //4.Shutter Button
+    private let shutterButton : UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        button.layer.cornerRadius = 50
+        button.layer.borderWidth = 10
+        button.layer.borderColor = UIColor.white.cgColor
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        view.layer.addSublayer(previewLayer)
+        view.addSubview(shutterButton)
+        
+        checkCameraPermission()
+        shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = view.bounds
+        shutterButton.center = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height - 100)
+    }
 
 }
 
+// MARK:  Private Methods
+private extension ViewController{
+    
+    func checkCameraPermission(){
+        switch AVCaptureDevice.authorizationStatus(for: .video){
+        case .notDetermined:
+            //Request
+            AVCaptureDevice.requestAccess(for: .video) { [weak self]granted in
+                guard granted else{
+                    return
+                }
+                DispatchQueue.main.async {
+                    self?.setUpCamera()
+                }
+            }
+        case.restricted:
+            break
+        case .denied:
+            break
+        case .authorized:
+            setUpCamera()
+        default:
+            break
+        }
+    }
+    
+    func setUpCamera(){
+        let session = AVCaptureSession()
+        if let device = AVCaptureDevice.default(for: .video){
+            do{
+                let input = try AVCaptureDeviceInput(device: device)
+                if session.canAddInput(input){
+                    session.addInput(input)
+                }
+                
+                if session.canAddOutput(output){
+                    session.addOutput(output)
+                }
+                
+                previewLayer.videoGravity = .resizeAspectFill
+                previewLayer.session = session
+                
+                session.startRunning()
+                self.session = session
+            }catch{
+                print(error)
+            }
+        }
+    }
+    
+    @objc func didTapTakePhoto(){
+        output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
+    }
+}
+
+
+extension ViewController : AVCapturePhotoCaptureDelegate{
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let data = photo.fileDataRepresentation() else{
+            return
+        }
+        let image = UIImage(data: data)
+        
+        session?.stopRunning()
+        
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
+        imageView.frame = view.bounds
+        view.addSubview(imageView)
+    }
+}
